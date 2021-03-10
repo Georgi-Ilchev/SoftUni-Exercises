d7cddd7ccaa899779b1b04ce853457856fcb83b7
Georgi Ilchev <goshicha1@gmail.com>
2021-03-10 11:36:05 +0200

    automappingobjs

---
+++ C#/C#-DataBase/Entity Framework Core/07. Auto Mapping Objects/07. Auto-Mapping-Objects-Project/FastFood.Core/MappingConfiguration/FastFoodProfile.cs
@@ -0,0 +1,60 @@
+﻿namespace FastFood.Core.MappingConfiguration
+{
+    using AutoMapper;
+    using FastFood.Core.ViewModels.Categories;
+    using FastFood.Core.ViewModels.Employees;
+    using FastFood.Core.ViewModels.Items;
+    using FastFood.Core.ViewModels.Orders;
+    using FastFood.Models;
+    using FastFood.Models.Enums;
+    using System;
+    using System.Globalization;
+    using ViewModels.Positions;
+
+    public class FastFoodProfile : Profile
+    {
+        public FastFoodProfile()
+        {
+            //Positions
+            this.CreateMap<CreatePositionInputModel, Position>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.PositionName));
+
+            this.CreateMap<Position, PositionsAllViewModel>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.Name));
+
+            //Employee
+            this.CreateMap<Position, RegisterEmployeeViewModel>()
+                .ForMember(x => x.PositionId, y => y.MapFrom(s => s.Id));
+
+            this.CreateMap<RegisterEmployeeInputModel, Employee>();
+
+            this.CreateMap<Employee, EmployeesAllViewModel>()
+                .ForMember(x => x.Position, y => y.MapFrom(x => x.Position.Name));
+
+            //Category
+            this.CreateMap<Category, CategoryAllViewModel>();
+
+            this.CreateMap<CreateCategoryInputModel, Category>()
+                .ForMember(x => x.Name, y => y.MapFrom(s => s.CategoryName));
+
+            //Item
+            this.CreateMap<Category, CreateItemViewModel>()
+                .ForMember(x => x.CategoryId, y => y.MapFrom(s => s.Id));
+
+            this.CreateMap<CreateItemInputModel, Item>();
+
+            this.CreateMap<Item, ItemsAllViewModels>()
+                .ForMember(x => x.Category, y => y.MapFrom(s => s.Category));
+
+            //Order
+            this.CreateMap<CreateOrderInputModel, Order>()
+                .ForMember(x => x.DateTime, y => y.MapFrom(s => DateTime.UtcNow))
+                .ForMember(x => x.Type, y => y.MapFrom(s => OrderType.ToGo));
+
+            this.CreateMap<Order, OrderAllViewModel>()
+                .ForMember(x => x.Employee, y => y.MapFrom(s => s.Employee.Name))
+                .ForMember(x => x.OrderId, y => y.MapFrom(s => s.Id))
+                .ForMember(x => x.DateTime, y => y.MapFrom(s => s.DateTime.ToString("D", CultureInfo.InvariantCulture)));
+        }
+    }
+}
---
+++ C#/C#-DataBase/Entity Framework Core/07. Auto Mapping Objects/07. Auto-Mapping-Objects-Project/FastFood.Data/FastFoodContextDesignTimeFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Text;
+
+namespace FastFood.Data
+{
+    public class FastFoodContextDesignTimeFactory : IDesignTimeDbContextFactory<FastFoodContext>
+    {
+        public FastFoodContext CreateDbContext(string[] args)
+        {
+            var builder = new DbContextOptionsBuilder<FastFoodContext>();
+            builder.UseSqlServer(@"Server=DESKTOP-L3ARJIL\\SQLEXPRESS;Database=FastFood;Trusted_Connection=True;MultipleActiveResultSets=true");
+            //builder.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+
+            return new FastFoodContext(builder.Options);
+        }
+    }
+}
