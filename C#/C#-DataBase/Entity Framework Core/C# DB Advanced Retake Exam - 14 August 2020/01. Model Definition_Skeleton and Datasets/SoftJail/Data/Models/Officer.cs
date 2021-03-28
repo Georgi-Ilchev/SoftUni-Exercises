dacc1042c7dc6326e2d74c683916347d845513d7
Georgi Ilchev <goshicha1@gmail.com>
2021-03-28 23:04:20 +0300

    exam

---
+++ C#/C#-DataBase/Entity Framework Core/C# DB Advanced Retake Exam - 14 August 2020/01. Model Definition_Skeleton and Datasets/SoftJail/Data/Models/Officer.cs
@@ -0,0 +1,33 @@
+﻿using SoftJail.Data.Models.Enums;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SoftJail.Data.Models
+{
+    public class Officer
+    {
+        public Officer()
+        {
+            this.OfficerPrisoners = new HashSet<OfficerPrisoner>();
+        }
+
+        public int Id { get; set; }
+
+        [Required]
+        public string FullName { get; set; }
+
+        public decimal Salary { get; set; }
+
+        [Required]
+        public Position Position { get; set; }
+
+        [Required]
+        public Weapon Weapon { get; set; }
+
+        public int DepartmentId { get; set; }
+
+        public Department Department { get; set; }
+
+        public ICollection<OfficerPrisoner> OfficerPrisoners { get; set; }
+    }
+}
---
+++ C#/C#-DataBase/Entity Framework Core/C# DB Advanced Retake Exam - 14 August 2020/01. Model Definition_Skeleton and Datasets/SoftJail/DataProcessor/ImportDto/DepartmentCellInputModel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace SoftJail.DataProcessor.ImportDto
+{
+    public class DepartmentCellInputModel
+    {
+        [Required]
+        [StringLength(25, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        public List<CellInputModel> Cells { get; set; }
+    }
+
+    public class CellInputModel
+    {
+        [Range(1, 1000)]
+        public int CellNumber { get; set; }
+
+        public bool HasWindow { get; set; }
+    }
+}
