a8365f23558ff5f9b9eb35b9d9aad19026972aa3
Georgi-Ilchev <68916191+Georgi-Ilchev@users.noreply.github.com>
2021-06-23 11:22:30 +0300

    work on BattleCards Exam

--- C#/C#-WEB/C#Web-Basics/CSharp-Web-Server-main/Apps/BattleCards/Controllers/CardsController.cs
+++ C#/C#-WEB/C#Web-Basics/CSharp-Web-Server-main/Apps/BattleCards/Controllers/CardsController.cs
@@ -62,12 +62,12 @@ namespace BattleCards.Controllers
 
             var card = new Card()
             {
-               Name = model.Name,
-               ImageUrl = model.ImageUrl,
-               Keyword = model.Keyword,
-               Attack = model.Attack,
-               Health = model.Health,
-               Description = model.Description
+                Name = model.Name,
+                ImageUrl = model.ImageUrl,
+                Keyword = model.Keyword,
+                Attack = model.Attack,
+                Health = model.Health,
+                Description = model.Description
             };
 
             this.data.Cards.Add(card);
@@ -82,6 +82,90 @@ namespace BattleCards.Controllers
             this.data.SaveChanges();
 
             return Redirect("/Cards/All");
+        }
+
+        [Authorize]
+        public HttpResponse AddToCollection(int cardId)
+        {
+            var userId = this.GetUserId();
+            var result = this.AddCardToUser(cardId, userId);
+
+            if (result == false)
+            {
+                return this.Error("You already have this card.");
+            }
+
+            return Redirect("/Cards/All");
+        }
+
+        [Authorize]
+        public HttpResponse RemoveFromCollection(int cardId)
+        {
+            var userId = this.GetUserId();
+            var result = this.RemoveCardFromUser(cardId, userId);
+
+            if (result == false)
+            {
+                return this.Error("You don't have this card in your collection");
+            }
+
+            return Redirect("/Cards/Collection");
+        }
+
+        [Authorize]
+        public HttpResponse Collection()
+        {
+            var userId = this.GetUserId();
+            var viewModel = this.data.UserCards
+                                     .Where(uc => uc.UserId == userId)
+                                     .Select(uc => new GetUserCardCollectionViewModel()
+                                     {
+                                         Id = uc.Card.Id,
+                                         Name = uc.Card.Name,
+                                         Image = uc.Card.ImageUrl,
+                                         Description = uc.Card.Description,
+                                         Keyword = uc.Card.Keyword,
+                                         Attack = uc.Card.Attack,
+                                         Health = uc.Card.Health
+                                     })
+                                     .ToList();
+
+            return this.View(viewModel);
+        }
+
+        public bool AddCardToUser(int cardId, string userId)
+        {
+            if (this.data.UserCards.Any(uc => uc.UserId == userId &&
+                                              uc.CardId == cardId))
+            {
+                return false;
+            }
+
+            this.data.UserCards.Add(new UserCard()
+            {
+                CardId = cardId,
+                UserId = userId,
+            });
+
+            this.data.SaveChanges();
+            return true;
+        }
+
+        public bool RemoveCardFromUser(int cardId, string userId)
+        {
+            var card = this.data.UserCards
+                                .FirstOrDefault(uc => uc.UserId == userId &&
+                                                      uc.CardId == cardId);
+
+            if (card == null)
+            {
+                return false;
+            }
+
+            this.data.UserCards.Remove(card);
+            this.data.SaveChanges();
+
+            return true;
         }
     }
 }
