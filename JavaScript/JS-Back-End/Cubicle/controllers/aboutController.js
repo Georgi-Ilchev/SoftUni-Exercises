b04861adb0a7d96335131b399d6ced9622faa3a3
Georgi-Ilchev <68916191+Georgi-Ilchev@users.noreply.github.com>
2022-01-05 18:05:02 +0200

    Adding page titles to layout

--- JavaScript/JS-Back-End/Cubicle/controllers/aboutController.js
+++ JavaScript/JS-Back-End/Cubicle/controllers/aboutController.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const router = Router();
 
 router.get('/', (req, res) => {
-    res.render('about')
+    res.render('about', {title: 'About'})
 });
 
 module.exports = router;
--- JavaScript/JS-Back-End/Cubicle/routes.js
+++ JavaScript/JS-Back-End/Cubicle/routes.js
@@ -6,7 +6,7 @@ const router = Router();
 router.use('/', productController);
 router.use('/about', aboutController);
 router.get('*', (req, res) => {
-    res.render('404');
+    res.render('404', {title: 'Not Found'});
 });
 
 module.exports = router;
