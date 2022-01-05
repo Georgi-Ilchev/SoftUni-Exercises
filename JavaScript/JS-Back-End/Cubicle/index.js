12f917f7a8658d95423543436aa8a25d55c59c8a
Georgi-Ilchev <68916191+Georgi-Ilchev@users.noreply.github.com>
2022-01-05 16:49:14 +0200

    Add routes

--- JavaScript/JS-Back-End/Cubicle/index.js
+++ JavaScript/JS-Back-End/Cubicle/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const config = require('./config/config.js');
+const routes = require('./routes.js');
 const app = express();
 const expressConfig = require('./config/express.js');
 
@@ -7,7 +8,5 @@ expressConfig(app);
 
 console.log(process.env.NODE_ENV);
 
-app.get('/', (req, res) => {
-    res.render('home', { layout: false });
-});
+app.use(routes);
 app.listen(config.PORT, () => console.log(`Server is running on port ${config.PORT}...`));
