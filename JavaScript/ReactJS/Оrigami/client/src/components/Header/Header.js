cec394dc1a846daa4491396f14446b0191a2ecf7
Georgi-Ilchev <68916191+Georgi-Ilchev@users.noreply.github.com>
2022-01-28 16:33:24 +0200

    Extract components from header

---
+++ JavaScript/ReactJS/Оrigami/client/src/components/Header/Header.js
@@ -0,0 +1,26 @@
+import style from './Header.module.css';
+import navStyle from './NavigationItem/NavigationItem.module.css';
+import NavigationItem from './NavigationItem/NavigationItem.js';
+
+const Header = () => {
+    return (
+        <nav className={style.navigation}>
+            <ul>
+                <li className={navStyle.listItem}><img src="/white-origami-bird.png" alt="white origami" /></li>
+                <NavigationItem>Going to 1</NavigationItem>
+                <NavigationItem>Going to 2</NavigationItem>
+                <NavigationItem>Going to 3</NavigationItem>
+                <NavigationItem>Going to 4</NavigationItem>
+                <NavigationItem>Going to 5</NavigationItem>
+                <NavigationItem>Going to 6</NavigationItem>
+                <NavigationItem>Going to 7</NavigationItem>
+                <NavigationItem>Going to 8</NavigationItem>
+                <NavigationItem>Going to 9</NavigationItem>
+                <NavigationItem>Going to 10</NavigationItem>
+                <NavigationItem>Going to 11</NavigationItem>
+            </ul>
+        </nav>
+    );
+};
+
+export default Header;
